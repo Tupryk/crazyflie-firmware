6f998e77a344b7a9952000b9e3d15de88f80068d
Wolfgang Hoenig <hoenig@tu-berlin.de>
2023-04-03 23:03:34 +0200

    fix build

--- src/deck/drivers/src/deck_electromagnet.c
+++ src/deck/drivers/src/deck_electromagnet.c
@@ -1,39 +1,41 @@
 #define DEBUG_MODULE "DeckElectromagnet"
 #include "debug.h"
 
-#include "deck_electromagnet.h"
+// #include "deck_electromagnet.h"
 #include "deck.h"
 
-const deckPin_t gatePin = DECK_GPIO_IO1;
+// const deckPin_t gatePin = DECK_GPIO_IO1;
+#define GATE_PIN    DECK_GPIO_IO1
+
 
 static void initializeDeckElectromagnet()
 {
 	DEBUG_PRINT("Initializing electromagnet deck!\n");
 
 	// Configure pin IO1 as an output with pull-down resistor
-	pinMode(gatePin, OUTPUT);
+	pinMode(GATE_PIN, OUTPUT);
 
 	// Test: Activate electromagnet
-	digitalWrite(gatePin, HIGH);
+	digitalWrite(GATE_PIN, HIGH);
 
 	DEBUG_PRINT("Electromagnet deck initialized successfully!\n");
 }
 
-static void activateDeckElectromagnet()
-{
-	DEBUG_PRINT("Activating electromagnet deck!\n");
+// static void activateDeckElectromagnet()
+// {
+// 	DEBUG_PRINT("Activating electromagnet deck!\n");
 
-	// Activate electromagnet
-	digitalWrite(gatePin, HIGH);
-}
+// 	// Activate electromagnet
+// 	digitalWrite(gatePin, HIGH);
+// }
 
-static void deactivateDeckElectromagnet()
-{
-	DEBUG_PRINT("Deactivating electromagnet deck!\n");
+// static void deactivateDeckElectromagnet()
+// {
+// 	DEBUG_PRINT("Deactivating electromagnet deck!\n");
 
-	// Deactivate electromagnet
-	digitalWrite(gatePin, LOW);
-}
+// 	// Deactivate electromagnet
+// 	digitalWrite(gatePin, LOW);
+// }
 
 static const DeckDriver driverElectromagnet = {
 	.vid = 0,
