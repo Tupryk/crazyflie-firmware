85074f5796ca1b79e6d328999622a021a2b240b7
Khaledwahba1994 <Khaled.hassan1994@gmail.com>
2023-04-03 16:12:07 +0200

    testing electromagnets, compiles but doesn't work

---
+++ src/drivers/interface/deck_electromagnet.h
@@ -0,0 +1,10 @@
+#ifndef __DECK_ELECTROMAGNET_H__
+#define __DECK_ELECTROMAGNET_H__
+
+static void initializeDeckElectromagnet();
+
+static void activateDeckElectromagnet();
+
+static void deactivateDeckElectromagnet();
+
+#endif
---
+++ src/drivers/src/deck_electromagnet.c
@@ -0,0 +1,53 @@
+#define DEBUG_MODULE "DeckElectromagnet"
+#include "debug.h"
+
+#include "deck_electromagnet.h"
+#include "deck.h"
+
+const deckPin_t gatePin = DECK_GPIO_IO1;
+
+static void initializeDeckElectromagnet()
+{
+	DEBUG_PRINT("Initializing electromagnet deck!\n");
+
+	// Configure pin IO1 as an output with pull-down resistor
+	pinMode(gatePin, OUTPUT);
+
+	// Test: Activate electromagnet
+	digitalWrite(gatePin, HIGH);
+
+	DEBUG_PRINT("Electromagnet deck initialized successfully!\n");
+}
+
+static void activateDeckElectromagnet()
+{
+	DEBUG_PRINT("Activating electromagnet deck!\n");
+
+	// Activate electromagnet
+	digitalWrite(gatePin, HIGH);
+}
+
+static void deactivateDeckElectromagnet()
+{
+	DEBUG_PRINT("Deactivating electromagnet deck!\n");
+
+	// Deactivate electromagnet
+	digitalWrite(gatePin, LOW);
+}
+
+static const DeckDriver driverElectromagnet = {
+	.vid = 0,
+  	.pid = 0,
+	.name = "deckElectromagnet",
+	.usedGpio = DECK_USING_IO_1,
+	.init = initializeDeckElectromagnet,
+};
+
+DECK_DRIVER(driverElectromagnet);
+
+// add a parameter to the deck driver
+// PARAM_GROUP_START(electromagnet)
+
+// PARAM_ADD(PARAM_BOOL, activate, &activateDeckElectromagnet)
+
+// PARAM_GROUP_STOP(electromagnet)
