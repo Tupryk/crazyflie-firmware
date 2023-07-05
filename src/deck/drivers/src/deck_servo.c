ebcc225dc878a95345ea9534a27fdc97928cf826
Dennis Schmidt <dennis.schmidt.0@web.de>
2023-07-05 17:48:11 +0200

    cleanup

--- src/deck/drivers/src/deck_servo.c
+++ src/deck/drivers/src/deck_servo.c
@@ -32,30 +32,27 @@
 
 #include "servo.h"
 
-// linear servo
-#define ratioOff 		0
-#define ratioHalf 		128
-#define ratioMax 		255
-
-// linear actuator (right) (shaft looking up, arm at top == 100% length, arm at bottom == 0% length)
-#define ratio_500us 	6 // 0.5 ms pulse at 50 Hz -> 100% length (GETS HOT, CRITICAL)
-#define ratio_700us 	9 // 0.7 ms pulse at 50 Hz -> 95% length (OK, COOL)
-#define ratio_750us 	10 // 0.75 ms pulse at 50 Hz -> 90% length
-#define ratio_850us 	11 // 0.85 ms pulse at 50 Hz -> ...
-#define ratio_950us 	12 // 0.95 ms pulse at 50 Hz -> ...
-#define ratio_1000us 	13 // 1.0 ms pulse at 50 Hz -> ...
-#define ratio_1500us 	19 // 1.5 ms pulse at 50 Hz -> ...
-#define ratio_2000us 	26 // 2.0 ms pulse at 50 Hz -> 20% length
-#define ratio_2300us 	29 // 2.3 ms pulse at 50 Hz -> 5% length
-#define ratio_2500us 	32 // 2.5 ms pulse at 50 Hz -> 0% length (OK, COOL)
+// linear servo ratios 
+// right servo definitions: shaft looking up -> arm at top == 100% length, arm at bottom == 0% length)
+// the following comments for the ratios apply for the right servo only
+#define ratio_500us 	6 	// 0.5 ms pulse at 50 Hz 	-> 100% length (GETS HOT, CRITICAL) -> (CABLES) BOTTOM |-----...----x| TOP (SHAFT)
+#define ratio_700us 	9 	// 0.7 ms pulse at 50 Hz 	-> 95% length (OK, COOL)			-> (CABLES) BOTTOM |-----...---x-| TOP (SHAFT)
+#define ratio_750us 	10 	// 0.75 ms pulse at 50 Hz 	-> 90% length						-> (CABLES) BOTTOM |-----...--x--| TOP (SHAFT)
+#define ratio_850us 	11 	// 0.85 ms pulse at 50 Hz 	-> ...								-> (CABLES) BOTTOM |-----...-----| TOP (SHAFT)
+#define ratio_950us 	12 	// 0.95 ms pulse at 50 Hz 	-> ...								-> (CABLES) BOTTOM |-----...-----| TOP (SHAFT)	
+#define ratio_1000us 	13 	// 1.0 ms pulse at 50 Hz 	-> ...								-> (CABLES) BOTTOM |-----...-----| TOP (SHAFT)
+#define ratio_1500us 	19 	// 1.5 ms pulse at 50 Hz 	-> ...								-> (CABLES) BOTTOM |-----...-----| TOP (SHAFT)
+#define ratio_2000us 	26 	// 2.0 ms pulse at 50 Hz 	-> 20% length						-> (CABLES) BOTTOM |----x...-----| TOP (SHAFT)
+#define ratio_2300us 	29 	// 2.3 ms pulse at 50 Hz 	-> 5% length						-> (CABLES) BOTTOM |-x---...-----| TOP (SHAFT)
+#define ratio_2500us 	32 	// 2.5 ms pulse at 50 Hz 	-> 0% length (OK, COOL)				-> (CABLES) BOTTOM |x----...-----| TOP (SHAFT)
 
 // ratio limits of the linear actuators (top == shaft end)
 #define ratioRightTop 		ratio_700us
 #define ratioRightBottom 	ratio_2300us
-#define ratioLeftTop 		ratio_2300us
-#define ratioLeftBottom 	ratio_850us
+#define ratioLeftTop 		ratio_2300us	// mirrored
+#define ratioLeftBottom 	ratio_850us		// mirrored, but with ratio_700us, the left servo gets crititcally hot
 
-// normal servo info
+// normal servo info (currently not in use)
 //  500 us -> 190 deg
 // 1000 us -> 135 deg
 // 1500 us -> 100 deg
@@ -64,8 +61,8 @@
 
 uint8_t ratio = 0;
 
-uint16_t frequencyDefault = 50;
 uint16_t frequency = 50;
+uint16_t frequencyDefault = 50;
 
 uint8_t state = 0;
 
@@ -92,8 +89,9 @@ static void testServo(void)
 {
 	DEBUG_PRINT("Starting servo deck test!\n");
 
-	servoSetRatio(ratioRightBottom);
 	servoSetFreq(frequencyDefault);
+	activateServo();  					// CLOSE GRIPPER with right servo
+	deactivateServo(); 					// OPEN GRIPPER with right servo
 
 	DEBUG_PRINT("Ending servo deck test!\n");
 }
@@ -101,9 +99,9 @@ static void testServo(void)
 static void activateCallback(void)
 {
 	if (state) {
-		activateServo();
+		activateServo(); 				// CLOSE GRIPPER with right servo
 	} else {
-		deactivateServo();
+		deactivateServo(); 				// OPEN GRIPPER with right servo
 	}
 }
 
@@ -111,16 +109,16 @@ static void activateServo()
 {	
 	DEBUG_PRINT("Activating servo!\n");
 
-	servoSetRatio(ratio_1000us);
 	servoSetFreq(frequencyDefault);
+	servoSetRatio(ratioRightBottom); 	// CLOSE GRIPPER with right servo
 }
 
 static void deactivateServo()
 {
 	DEBUG_PRINT("Deactivating servo!\n");
 
-	servoSetRatio(ratio_1000us);
 	servoSetFreq(frequencyDefault);
+	servoSetRatio(ratioRightTop); 		// OPEN GRIPPER with right servo
 }
 
 static void setRatioCallback(void)
