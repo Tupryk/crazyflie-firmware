c65f0a14802ca49cc456f50081bc81922fa97bc2
Eckart Cobo Briesewitz <eck4art@gmail.com>
2025-02-20 11:03:13 +0100

    added new nn

--- src/modules/interface/controller/nn.h
+++ src/modules/interface/controller/nn.h
@@ -4,9 +4,8 @@
 #define __GEN_NN__
 
 #define INPUT_SIZE 12
-#define OUTPUT_SIZE 2
+#define OUTPUT_SIZE 6
 
 const float* nn_forward(float input[INPUT_SIZE]);
-const float* nn_forward_payload(float input[INPUT_SIZE]);
 
 #endif
