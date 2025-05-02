4bb47d52d9fb226f4f635ddb45bf5e8653ece868
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-05-02 17:18:30 +0200

    update model

--- vendor/stm32cubeai/network.c
+++ vendor/stm32cubeai/network.c
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    network.c
   * @author  AST Embedded Analytics Research Platform
-  * @date    2025-04-29T12:45:37+0000
+  * @date    2025-05-02T15:16:18+0000
   * @brief   AI Tool Automatic Code Generator for Embedded NN computing
   ******************************************************************************
   * @attention
@@ -35,14 +35,14 @@
 #define AI_NET_OBJ_INSTANCE g_network
  
 #undef AI_NETWORK_MODEL_SIGNATURE
-#define AI_NETWORK_MODEL_SIGNATURE     "0xbd024e2775dd4e80a7b96dbf2f4a41e7"
+#define AI_NETWORK_MODEL_SIGNATURE     "0xccdfdadf4b4c28a5b658bcd04c039e07"
 
 #ifndef AI_TOOLS_REVISION_ID
 #define AI_TOOLS_REVISION_ID     ""
 #endif
 
 #undef AI_TOOLS_DATE_TIME
-#define AI_TOOLS_DATE_TIME   "2025-04-29T12:45:37+0000"
+#define AI_TOOLS_DATE_TIME   "2025-05-02T15:16:18+0000"
 
 #undef AI_TOOLS_COMPILE_TIME
 #define AI_TOOLS_COMPILE_TIME    __DATE__ " " __TIME__
@@ -301,7 +301,7 @@ AI_NETWORK_OBJ_DECLARE(
     512, NULL, NULL),
   AI_TENSOR_LIST_IO_OBJ_INIT(AI_FLAG_NONE, AI_NETWORK_IN_NUM, &serving_default_args_00_output),
   AI_TENSOR_LIST_IO_OBJ_INIT(AI_FLAG_NONE, AI_NETWORK_OUT_NUM, &gemm_4_output),
-  &gemm_0_layer, 0x70c90776, NULL)
+  &gemm_0_layer, 0x01be0f85, NULL)
 
 #else
 
@@ -321,7 +321,7 @@ AI_NETWORK_OBJ_DECLARE(
   ),
   AI_TENSOR_LIST_IO_OBJ_INIT(AI_FLAG_NONE, AI_NETWORK_IN_NUM, &serving_default_args_00_output),
   AI_TENSOR_LIST_IO_OBJ_INIT(AI_FLAG_NONE, AI_NETWORK_OUT_NUM, &gemm_4_output),
-  &gemm_0_layer, 0x70c90776, NULL)
+  &gemm_0_layer, 0x01be0f85, NULL)
 
 #endif	/*(AI_TOOLS_API_VERSION < AI_TOOLS_API_VERSION_1_5)*/
 
@@ -432,7 +432,7 @@ ai_bool ai_network_get_info(
       .params            = AI_STRUCT_INIT,
       .activations       = AI_STRUCT_INIT,
       .n_nodes           = 0,
-      .signature         = 0x70c90776,
+      .signature         = 0x01be0f85,
     };
 
     if (!ai_platform_api_get_network_report(network, &r)) return false;
@@ -480,7 +480,7 @@ ai_bool ai_network_get_report(
       .map_weights       = AI_STRUCT_INIT,
       .map_activations   = AI_STRUCT_INIT,
       .n_nodes           = 0,
-      .signature         = 0x70c90776,
+      .signature         = 0x01be0f85,
     };
 
     if (!ai_platform_api_get_network_report(network, &r)) return false;
--- vendor/stm32cubeai/network_config.h
+++ vendor/stm32cubeai/network_config.h
@@ -3,7 +3,7 @@
   ******************************************************************************
   * @file    network_config.h
   * @author  AST Embedded Analytics Research Platform
-  * @date    2025-04-29T12:45:37+0000
+  * @date    2025-05-02T15:16:18+0000
   * @brief   AI Tool Automatic Code Generator for Custom Layers Implementation
   ******************************************************************************
   * @attention
