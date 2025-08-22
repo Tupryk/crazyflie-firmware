3812788b5e3b27abd4467bb2e30cd1b25b053da6
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-08-22 17:27:50 +0200

    no last action

--- vendor/stm32cubeai/network_data.c
+++ vendor/stm32cubeai/network_data.c
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    network_data.c
   * @author  AST Embedded Analytics Research Platform
-  * @date    2025-08-22T14:13:54+0000
+  * @date    2025-08-22T15:21:15+0000
   * @brief   AI Tool Automatic Code Generator for Embedded NN computing
   ******************************************************************************
   * @attention
