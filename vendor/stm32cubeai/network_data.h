28b3b0574958986ad5f9a599db658669e94a9386
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-08-25 18:48:30 +0200

    new policy with random delay

--- vendor/stm32cubeai/network_data.h
+++ vendor/stm32cubeai/network_data.h
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    network_data.h
   * @author  AST Embedded Analytics Research Platform
-  * @date    2025-08-25T14:38:32+0000
+  * @date    2025-08-25T16:45:24+0000
   * @brief   AI Tool Automatic Code Generator for Embedded NN computing
   ******************************************************************************
   * Copyright (c) 2025 STMicroelectronics.
