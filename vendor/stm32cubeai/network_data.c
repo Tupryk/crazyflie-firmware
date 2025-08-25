2e1ecfeb9c7ac30e9fdba93bf994e452ccfa9300
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-08-25 17:16:23 +0200

    higher motor delay

--- vendor/stm32cubeai/network_data.c
+++ vendor/stm32cubeai/network_data.c
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    network_data.c
   * @author  AST Embedded Analytics Research Platform
-  * @date    2025-08-25T10:06:12+0000
+  * @date    2025-08-25T14:38:32+0000
   * @brief   AI Tool Automatic Code Generator for Embedded NN computing
   ******************************************************************************
   * @attention
