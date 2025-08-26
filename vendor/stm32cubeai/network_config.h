dd71ab552d890cd7e8291517a156a190f55204a0
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-08-26 17:35:26 +0200

    singel quad with payload

--- vendor/stm32cubeai/network_config.h
+++ vendor/stm32cubeai/network_config.h
@@ -3,7 +3,7 @@
   ******************************************************************************
   * @file    network_config.h
   * @author  AST Embedded Analytics Research Platform
-  * @date    2025-08-26T08:38:09+0000
+  * @date    2025-08-26T15:33:07+0000
   * @brief   AI Tool Automatic Code Generator for Custom Layers Implementation
   ******************************************************************************
   * @attention
