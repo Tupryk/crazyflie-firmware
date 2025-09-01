b56d91dff6bac5ac0873f0621fa7f31fd3836525
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-09-01 13:42:28 +0200

    more level policy

--- vendor/stm32cubeai/network.h
+++ vendor/stm32cubeai/network.h
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    network.h
   * @author  AST Embedded Analytics Research Platform
-  * @date    2025-09-01T09:37:59+0000
+  * @date    2025-09-01T11:41:44+0000
   * @brief   AI Tool Automatic Code Generator for Embedded NN computing
   ******************************************************************************
   * @attention
@@ -23,7 +23,7 @@
 
 /******************************************************************************/
 #define AI_NETWORK_MODEL_NAME          "network"
-#define AI_NETWORK_ORIGIN_MODEL_NAME   "actor_model88"
+#define AI_NETWORK_ORIGIN_MODEL_NAME   "actor_model90"
 
 /******************************************************************************/
 #define AI_NETWORK_ACTIVATIONS_ALIGNMENT   (4)
