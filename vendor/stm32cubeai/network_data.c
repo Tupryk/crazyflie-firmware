48821b88dbd725b457c00b7e5c573b7cec2d85cd
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-09-01 11:40:25 +0200

    policy without rp penalty

--- vendor/stm32cubeai/network_data.c
+++ vendor/stm32cubeai/network_data.c
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    network_data.c
   * @author  AST Embedded Analytics Research Platform
-  * @date    2025-09-01T09:16:37+0000
+  * @date    2025-09-01T09:37:59+0000
   * @brief   AI Tool Automatic Code Generator for Embedded NN computing
   ******************************************************************************
   * @attention
