f330c5d7aae8bf7ef23fd163164d5cdab58e995f
Khaled Wahba <khaled.hassan1994@gmail.com>
2025-10-06 14:01:15 +0200

    update gains

--- src/modules/src/controller/controller_lee.c
+++ src/modules/src/controller/controller_lee.c
@@ -62,16 +62,16 @@ static controllerLee_t g_self = {
   .J = {16.571710e-6, 16.655602e-6, 29.261652e-6}, // kg m^2
 
   // Position PID
-  .Kpos_P = {10.0, 10.0, 10.0}, // Kp in paper
+  .Kpos_P = {12.0, 12.0, 12.0}, // Kp in paper
   .Kpos_P_limit = 100,
-  .Kpos_D = {9.0, 9.0, 9.0}, // Kv in paper
+  .Kpos_D = {10.5, 10.5, 10.5}, // Kv in paper
   .Kpos_D_limit = 100,
   .Kpos_I = {5.0, 5.0, 5.0}, // not in paper
-  .Kpos_I_limit = 2.0,
+  .Kpos_I_limit = 100,
 
   // Attitude PID
-  .KR = {0.0065, 0.0065, 0.008},
-  .Komega = {0.00095, 0.00095, 0.001},
+  .KR = {0.007, 0.007, 0.008},
+  .Komega = {0.0016, 0.0016, 0.002},
   .KI = {0.01, 0.01, 0.01},
 
   // INDI
